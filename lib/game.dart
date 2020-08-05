6b361e77ca52b9c365c38e3ae34dfeb859297723
RomjanHossain <romjanhossain726526@gmail.com>
2020-08-05 10:23:23 +0600

    roll dice function added

--- lib/game.dart
+++ lib/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class GamePage extends StatefulWidget {
   @override
@@ -6,6 +7,16 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  Random random = new Random();
+  int first = 1;
+  int second = 1;
+  void rollDice() {
+    setState(() {
+      first = random.nextInt(6) + 1;
+      second = random.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,7 +27,9 @@ class _GamePageState extends State<GamePage> {
             child: Padding(
               padding: const EdgeInsets.only(top: 5),
               child: FloatingActionButton.extended(
-                onPressed: () {},
+                onPressed: () {
+                  print('New Game btn pressed');
+                },
                 label: Text('New Game'),
               ),
             ),
@@ -26,21 +39,63 @@ class _GamePageState extends State<GamePage> {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Image.asset('images/dice2.png'),
+                  child: Image.asset('images/dice$first.png'),
                 ),
               ),
               Container(
-                // color: Colors.red,
-                child: Text('center'),
+                // color: Colors.white,
+                width: 70,
+                height: 70,
+                child: Center(
+                    child: Text(
+                  '69',
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                decoration:
+                    BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
               ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Image.asset('images/dice1.png'),
+                  child: Image.asset('images/dice$second.png'),
                 ),
               ),
-              // Text("what")
             ],
+          ),
+          Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              // color: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          rollDice();
+                        },
+                        label: Text('Roll Dice'),
+                      ),
+                    ),
+                    FloatingActionButton.extended(
+                      onPressed: () {},
+                      label: Text('Hold'),
+                    ),
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  // shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(50)),
+            ),
           ),
         ],
       ),
